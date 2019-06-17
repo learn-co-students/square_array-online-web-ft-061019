570cbd7e0a9e8bc3eb0d9e13dfc863ea01f058dc
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-17 21:19:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,23 @@
-def square_array(array)
-  squared = []
-  array.each { |element| squared << element ** 2 }
-  squared
+=begin
+I know this is the soltuon off github
+def square_array(numbers)
+  new_numbers = []
+  numbers.each { |number| new_numbers << number ** 2 }
+  new_numbers
 end
+=end 
+
+def square_array(array)
+ new_array = []
+    array.each do |number|
+      new_array << number **2 
+    end
+  new_array
+end
+
+
+def xsquare_array(array)
+    array.collect { |number| number **2 }
+    array
+end
+
