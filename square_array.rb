5a2c8b0a8863ee2cbd1bcb67154da40a95002c73
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-17 20:29:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
+=begin
+# def square_array(array)
+  numbers = [1, 2, 3]
+ # array = square_array(numbers)
+  numbers.each do |number|
+  new_numbers = number**2
+ # numbers = number**2
+  puts "#{number}"
+   puts "#{new_numbers}"
+   end
+ # end
+ end
+=end 
 def square_array(array)
-  # your code here
-end
+numbers = [1, 2, 3]
+# square_array(numbers)
+  numbers.each do |number|
+  new_numbers = number**2
+ # numbers = number**2
+  puts "#{number}"
+   puts "#{new_numbers}"
+   end
