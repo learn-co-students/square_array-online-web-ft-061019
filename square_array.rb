67c473a9ac509fc433414a2b4665a3107e5b043a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-18 05:40:55 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,23 +1,13 @@
-=begin
-I know this is the soltuon off github
-def square_array(numbers)
-  new_numbers = []
-  numbers.each { |number| new_numbers << number ** 2 }
-  new_numbers
-end
-=end 
-
 def square_array(array)
- new_array = []
-    array.each do |number|
-      new_array << number **2 
-    end
+  new_array = []
+  array.each {|element| new_array << element ** 2   }
   new_array
+  puts "#{new_array}"
 end
 
+numbers = [1,2,3]
+square_array(numbers)
 
-def xsquare_array(array)
-    array.collect { |number| number **2 }
-    array
-end
+new_numbers = [9,10,16,25]
 
+square_array(new_numbers)
